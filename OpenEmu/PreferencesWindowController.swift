4b0ba7b5463ad0fce9c61356a5d27f5e45d846c2
shokuninstudio <54727267+shokuninstudio@users.noreply.github.com>
2026-05-22 17:23:29 +0100

    Make Preferences window floating (#578)

--- OpenEmu/PreferencesWindowController.swift
+++ OpenEmu/PreferencesWindowController.swift
@@ -57,6 +57,7 @@ final class PreferencesWindowController: NSWindowController {
     override func windowDidLoad() {
         
         super.windowDidLoad()
+        window?.level = .floating // make Settings window floating so it always stays on top
         
         preferencesTabViewController = PreferencesTabViewController()
         self.window?.contentView = preferencesTabViewController.view
